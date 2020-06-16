6d8d0c1902227a931e26a32063eb1855a95df94b
NobodysBuddy <aryan.aparna.thakur@gmail.com>
2020-06-16 19:49:43 +0530

    Test Testing...

--- redfoxui-source/src/App.js
+++ redfoxui-source/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <h1>This Page Doesn't Have Any UI Content</h1>
         <h3>This Page Is The RedfoxUI Soucre Code And Thus Doesn't Have Any UI Content Here. :)</h3>
         <CoreComponent />
+        <h2>Testing...</h2>
       </header>
     </div>
   );
