294d6948fc12c74512e42d36c1fd955a9f19ed61
NobodysBuddy <aryan.aparna.thakur@gmail.com>
2020-06-16 19:43:38 +0530

    Just Testing...

---
+++ redfoxui-source/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This Page Doesn't Have Any UI Content</h1>
+        <h3>This Page Is The RedfoxUI Soucre Code And Thus Doesn't Have Any UI Content Here. :)</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ redfoxui-source/src/Core/CoreComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Core extends React.Component {
+    render() {
+        return (
+            <h1 className="CoreComponent">React Core In Development...</h1>
+        );
+    }    
+}
+
+export default Core;
