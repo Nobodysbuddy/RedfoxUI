348044b9686d4a0b327a5244a8a1e0467153a33f
NobodysBuddy <aryan.aparna.thakur@gmail.com>
2020-06-17 17:24:28 +0530

    The Edit

--- redfoxui-source/src/App.js
+++ redfoxui-source/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import CoreComponent from './Core/CoreComponent';
+import RfxText from './Core/Widgets/Text/Text'
+
+var sampleText = 'Hello World!';
 
 function App() {
   return (
@@ -9,7 +12,11 @@ function App() {
         <h1>This Page Doesn't Have Any UI Content</h1>
         <h3>This Page Is The RedfoxUI Soucre Code And Thus Doesn't Have Any UI Content Here. :)</h3>
         <CoreComponent />
-        <h2>Testing...</h2>
+        <div>
+          <h1>A Demo Of rfxText:</h1>
+          <hr />
+          <RfxText text={sampleText}/>
+        </div>
       </header>
     </div>
   );
